d8f994fbf7730b5a0fb85e6e7d7e169e93a95c1b
Rajya-Lakshmi-2003 <94794898+Rajya-Lakshmi-2003@users.noreply.github.com>
2023-03-23 15:11:42 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,18 @@
+function add(x,y){
+    return x+y;
+};
+function sub(x,y){
+    return x-y;
+};
+function mul(x,y){
+    return x*y;
+};
+function div(x,y){
+    return x/y;
+};
+exports.add1=add;
+exports.sub1=sub;
+exports.mul1=mul;
+exports.div1=div;
+
+
